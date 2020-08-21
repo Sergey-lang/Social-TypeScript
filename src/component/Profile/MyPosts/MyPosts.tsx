aa6cb9c1912a2014cf47551576715d7515a497ce
Sergey-KA <sergei.kuharyonok@yandex.by>
2020-08-21 16:12:49 +0300

    style for posts

--- src/component/Profile/MyPosts/MyPosts.tsx
+++ src/component/Profile/MyPosts/MyPosts.tsx
@@ -4,8 +4,19 @@ import style from './MyPosts.module.css';
 function MyPosts() {
   return (
     <div className={style.post_wrapper}>
-      <div className={style.my_posts}>my_posts</div>
-      <div className={style.add_new_posts}>add_new_posts</div>
+      <div className={style.any}></div>
+      <div className={style.wall_posts}>
+
+        <div className={style.add_new_posts}>
+          <div className={style.text_wrapper}>
+            <div className={style.small_user_photo}>img</div>
+            <textarea></textarea>
+          </div>
+            <button className={style.add_post_button}>Published</button>
+        </div>
+
+        <div className={style.my_posts}>my_posts</div>
+      </div>
     </div>
   )
 }
