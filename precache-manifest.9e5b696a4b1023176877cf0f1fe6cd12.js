94d60f843f2e02c06a651d2cd2bff676e293150a
Sergey <sergei.kuharyonok@yandex.by>
2020-12-08 14:39:55 +0300

    Updates

---
+++ precache-manifest.9e5b696a4b1023176877cf0f1fe6cd12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c0ff749a3e68513314138992b636df",
+    "url": "/Social-TypeScript/index.html"
+  },
+  {
+    "revision": "5abf15cf2fe908158f9e",
+    "url": "/Social-TypeScript/static/css/main.8b32b8ac.chunk.css"
+  },
+  {
+    "revision": "e5a7d145b313230b3a7b",
+    "url": "/Social-TypeScript/static/js/2.32b943b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Social-TypeScript/static/js/2.32b943b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5abf15cf2fe908158f9e",
+    "url": "/Social-TypeScript/static/js/main.4863eddd.chunk.js"
+  },
+  {
+    "revision": "42dc512b95ed467b0196",
+    "url": "/Social-TypeScript/static/js/runtime-main.18e493ad.js"
+  },
+  {
+    "revision": "9dc3825792d508bf23ef411d8e0ca7e9",
+    "url": "/Social-TypeScript/static/media/icons8_user.9dc38257.png"
+  },
+  {
+    "revision": "5861c44db520a839676b8df01f7c2213",
+    "url": "/Social-TypeScript/static/media/preloader.5861c44d.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Social-TypeScript/precache-manifest.e02499e84d980e3e5ae3131dae66f0aa.js"
+  "/Social-TypeScript/precache-manifest.9e5b696a4b1023176877cf0f1fe6cd12.js"
 );
 
 self.addEventListener('message', (event) => {
