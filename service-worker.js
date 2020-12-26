c9b16e7fc8ebd611f4d800963484f63c62261735
Sergey <sergei.kuharyonok@yandex.by>
2020-12-26 17:26:22 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Social-TypeScript/precache-manifest.b338776516f6ea9d490d66c30a825f5d.js"
+  "/Social-TypeScript/precache-manifest.8c7d2d036cd85cec6320d358dba33f9f.js"
 );
 
 self.addEventListener('message', (event) => {
